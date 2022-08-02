be875b431f41dbe805200c4ad9e20352f483438d
linleckkyalsin <linleckkyalsin@gmail.com>
2022-08-02 10:28:37 +0630

    feat: クラスを作成し、studentsを表示する処理を記載

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Course {
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Course {
+    private String name;
+    private int months;
+    private int fees;
+    private LocalDate startDate;
+
+    public Course(String name, int months, int fees, LocalDate startDate) {
+        this.name = name;
+        this.months = months;
+        this.fees = fees;
+        this.startDate = startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public int getFees() {
+        return fees;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+        Course course = (Course) o;
+        return months == course.months && fees == course.fees && name.equals(course.name) && startDate.equals(course.startDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, months, fees, startDate);
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Student {
+import java.util.List;
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private List<Course> courses;
+    private int age;
+    private List<String> basicKnowledges;
+
+    public Student(String name, List<Course> courses, int age, List<String> basicKnowledges) {
+        this.name = name;
+        this.courses = courses;
+        this.age = age;
+        this.basicKnowledges = basicKnowledges;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getBasicKnowledges() {
+        return basicKnowledges;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return age == student.age && name.equals(student.name) && courses.equals(student.courses) && basicKnowledges.equals(student.basicKnowledges);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, courses, age, basicKnowledges);
+    }
 }
